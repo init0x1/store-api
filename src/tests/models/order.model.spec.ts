2d5a8d9e9e14ab4b6e682d83ea8d17fd81eacc20
0xAbdoAli <abdelrahman.elshaikh7@gmail.com>
2023-07-11 14:12:13 +0300

    Adding orderModel Tests

--- src/tests/models/order.model.spec.ts
+++ src/tests/models/order.model.spec.ts
@@ -1,78 +1,93 @@
-/*
-import { OrderModel } from '../../models/order.model';
+import { OrderModel } from '../../models/order.model'
 import { Order } from '../../types/OrderType'
-import { UserModel } from '../../models/user.model';
+import { UserModel } from '../../models/user.model'
 import { User } from '../../types/UserType'
-import Client from '../../database';
+import Client from '../../database'
 
-const orderModel = new OrderModel();
-const userModel = new UserModel();
+const orderModel = new OrderModel()
+const userModel = new UserModel()
 
-describe('Order Model', () => {
-  let createdUser: User;
-  let createdOrder: Order;
+describe('Order Model Test', () => {
+  let createdUser: User
+  let createdOrder0: Order
+  let createdOrder1: Order
 
   beforeAll(async () => {
-    await Client.connect();
-
-    // Create a user
-    const user: User = {
-      email: 'test@example.com',
-      password: 'password123',
-      first_name: 'John',
-      last_name: 'Doe',
-    };
-    createdUser = await userModel.create(user);
-
-  });
+    createdUser = await userModel.create({
+      email: 'test@0xabdo.com',
+      password: 'letmein',
+      first_name: 'Abdo',
+      last_name: 'Ali'
+    })
+  })
 
   afterAll(async () => {
-    await orderModel.delete(createdOrder.order_id as string);
-    await userModel.delete(createdUser.user_id as string);
-    await Client.end();
-  });
+    const connection = await Client.connect()
+    await connection.query(`DELETE FROM orders;`)
+    await connection.query(`DELETE FROM users;`)
+    connection.release()
+  })
+
+  it('should have a getAll method', () => {
+    expect(orderModel.getAll).toBeDefined()
+  })
+
+  it('should have a getById method', () => {
+    expect(orderModel.getById).toBeDefined()
+  })
+
+  it('should have a create method', () => {
+    expect(orderModel.create).toBeDefined()
+  })
+
+  it('should have an update method', () => {
+    expect(orderModel.update).toBeDefined()
+  })
+
+  it('should have a delete method', () => {
+    expect(orderModel.delete).toBeDefined()
+  })
 
   it('should create a new order', async () => {
-    const order: Order = {
-      user_id: createdUser.user_id as string,
-      order_status: 'pending',
-    };
-    createdOrder = await orderModel.create(order);
+    const orderToCreate: Order = {
+      order_status: 'not Delivered',
+      user_id: createdUser.user_id as string
+    }
+    createdOrder0 = await orderModel.create(orderToCreate)
 
-    expect(createdOrder.user_id).toEqual(order.user_id);
-    expect(createdOrder.order_status).toEqual(order.order_status);
-    expect(createdOrder.order_id).toBeDefined();
-  });
-
-  it('should retrieve an order by ID', async () => {
-    const retrievedOrder = await orderModel.getById(createdOrder.order_id as string);
-
-    expect(retrievedOrder).toEqual(createdOrder);
-  });
+    expect(createdOrder0.order_status).toBe(orderToCreate.order_status)
+    expect(createdOrder0.user_id).toBe(orderToCreate.user_id)
+  })
 
   it('should retrieve all orders', async () => {
-    const orders = await orderModel.getAll();
+    const orders = await orderModel.getAll()
+    expect(orders.length).toBe(1)
+    expect(orders[0].order_status).toBe(createdOrder0.order_status)
+    expect(orders[0].user_id).toBe(createdOrder0.user_id)
+    expect(orders[0].order_id).toBe(createdOrder0.order_id)
+  })
 
-    expect(orders.length).toBeGreaterThan(0);
-  });
+  it('should retrieve an order by ID', async () => {
+    const order = await orderModel.getById(createdOrder0.order_id as string)
+    expect(order.order_id).toBe(createdOrder0.order_id)
+    expect(order.user_id).toBe(createdOrder0.user_id)
+    expect(order.order_status).toBe(createdOrder0.order_status)
+  })
 
-  it('should update an existing order', async () => {
-    const updatedOrder: Order = {
-      order_id: createdOrder.order_id,
-      user_id: createdOrder.user_id,
-      order_status: 'completed',
-    };
-    const updated = await orderModel.update(updatedOrder);
+  it('should update an order', async () => {
+    const orderToUpdate: Order = {
+      order_id: createdOrder0.order_id,
+      order_status: 'Delivered',
+      user_id: createdUser.user_id as string
+    }
+    const updatedOrder = await orderModel.update(orderToUpdate)
 
-    expect(updated.order_id).toEqual(updatedOrder.order_id);
-    expect(updated.order_status).toEqual(updatedOrder.order_status);
-  });
+    expect(updatedOrder.order_status).toBe(orderToUpdate.order_status)
+  })
 
   it('should delete an order', async () => {
-    await orderModel.delete(createdOrder.order_id as string);
-    const deletedOrder = await orderModel.getById(createdOrder.order_id as string);
-
-    expect(deletedOrder).toBeNull();
-  });
+    await orderModel.delete(createdOrder0.order_id as string)
+    const orders = await orderModel.getAll()
+    expect(orders.length).toBe(0)
+  })
 })
-*/
